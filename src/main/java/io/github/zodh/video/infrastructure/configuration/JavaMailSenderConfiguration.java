17f83ac266e95cece467a7e15955dea5055ce8aa
Felipe Santos <felipe.carvalho.bs.contato@gmail.com>
2025-04-16 22:22:20 -0300

    fix email configs

--- src/main/java/io/github/zodh/video/infrastructure/configuration/JavaMailSenderConfiguration.java
+++ src/main/java/io/github/zodh/video/infrastructure/configuration/JavaMailSenderConfiguration.java
@@ -1,15 +1,38 @@
 package io.github.zodh.video.infrastructure.configuration;
 
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class JavaMailSenderConfiguration {
 
+
+  @Value("${spring.mail.host}")
+  private String host;
+  @Value("${spring.mail.port}")
+  private String port;
+  @Value("${spring.mail.username}")
+  private String username;
+  @Value("${spring.mail.password}")
+  private String password;
+
   @Bean
-  public JavaMailSender getJavaMailSender() {
+  public JavaMailSenderImpl getJavaMailSender() {
+    JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+    javaMailSender.setHost(host);
+    javaMailSender.setPort(Integer.parseInt(port));
+    javaMailSender.setUsername(username);
+    javaMailSender.setPassword(password);
+
+    Properties props = javaMailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+
     return new JavaMailSenderImpl();
   }
 
